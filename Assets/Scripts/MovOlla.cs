348fa96d8598ad65ed395a9074a51d397294571b
juan <juampats12345@gmail.com>
2024-05-28 18:53:42 +0200

    script de rotacion de la olla

--- Assets/Scripts/MovOlla.cs
+++ Assets/Scripts/MovOlla.cs
@@ -6,59 +6,66 @@ using UnityEngine.UI;
 
 public class MovOlla : MonoBehaviour
 {
-    
+
     // Start is called before the first frame update
-    int Giro=0; //Giro derecha o izquierda
-    float ContadorCambio=6.0f; //Cambia de estado
-    public float VelocidadGiro= 0.0f; 
-    public float VelocidadMovimiento= 0.0f;
+    int Giro = 0; //Giro derecha o izquierda
+    float ContadorCambio = 6.0f; //Cambia de estado
+    public float VelocidadGiro = 0.0f;
+    public float VelocidadMovimiento = 0.0f;
     private Vector3 startPosition; // Posición inicial
-    void Start()   
+    void Start()
     {
-        float ContadorSeg=3.0f;
-        float currentTime = ContadorSeg; 
-        while (currentTime >0) 
+        float ContadorSeg = 3.0f;
+        float currentTime = ContadorSeg;
+        while (currentTime > 0)
         {
             Debug.Log("Tiempo restante: " + Mathf.Ceil(currentTime).ToString() + "s");
             currentTime -= Time.deltaTime;
         }
-      VelocidadGiro= 100.0f;
-      VelocidadMovimiento=1.0f;
+        VelocidadGiro = 100.0f;
+        VelocidadMovimiento = 1.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float currentTime = ContadorCambio; 
-        while (currentTime >0){
-            Giro = Cambio(0,Giro);
-        } Giro= Cambio(1, Giro);
-        
+        float currentTime = ContadorCambio;
+        while (currentTime > 0)
+        {
+            Giro = Cambio(0, Giro);
+        }
+        Giro = Cambio(1, Giro);
+
     }
 
-    int Cambio (int NG,int Giro){
+    int Cambio(int NG, int Giro)
+    {
 
-        if(NG==1){
-        Giro= Random.Range(0,9);        
+        if (NG == 1)
+        {
+            Giro = Random.Range(0, 9);
             if (Giro > 4) //sentido horario
-            {   
-                transform.Rotate(Vector3.up, VelocidadGiro * Time.deltaTime);
-            ContadorCambio=6.0f;
-            }else //sentido antihorario
             {
-                transform.Rotate(Vector3.up, -( VelocidadGiro * Time.deltaTime));
-            ContadorCambio=6.0f;
+                transform.Rotate(Vector3.up, VelocidadGiro * Time.deltaTime);
+                ContadorCambio = 6.0f;
+            }
+            else //sentido antihorario
+            {
+                transform.Rotate(Vector3.up, -(VelocidadGiro * Time.deltaTime));
+                ContadorCambio = 6.0f;
             };
             return Giro;
-        }else{
+        }
+        else
+        {
             transform.Rotate(Vector3.up, VelocidadGiro * Time.deltaTime);
             return Giro;
         }
     }
-   /* void Movimiento()
-    {
-        float RangoMax = 5.0f; // Rango de movimiento maximo
+    /* void Movimiento()
+     {
+         float RangoMax = 5.0f; // Rango de movimiento maximo
 
 
-    }*/
+     }*/
 }
---
+++ Assets/Scripts/MovOllaV2.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class MovOllaV2 : MonoBehaviour
+{
+    public float velocidadOlla;
+    public float tiempoCambiarDir;
+
+    private float contadorCambioSentido;
+    private int isMovHorario;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMovHorario = 1;
+        contadorCambioSentido = tiempoCambiarDir/2;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        contadorCambioSentido -= Time.deltaTime;
+
+        if( contadorCambioSentido < 0 )
+        {
+            contadorCambioSentido = tiempoCambiarDir;
+            isMovHorario = -isMovHorario;
+        }
+
+        //transform.Rotate(Vector3.up, currSpeedOlla * Time.deltaTime);
+        rb.AddRelativeTorque(Vector3.down * velocidadOlla * Time.deltaTime * isMovHorario );
+    }
+}
