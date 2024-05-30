5249c458747e3a3642e7d05e16b09f64280da5b1
juan <juampats12345@gmail.com>
2024-05-30 19:12:01 +0200

    los enemigos se ponen en fila al perder

--- Assets/Scripts/KillZone.cs
+++ Assets/Scripts/KillZone.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -10,30 +11,33 @@ public class KillZone : MonoBehaviour
     public GameObject hasPerdido, hasGanado;
     public TextMeshProUGUI hasGanadoText;
     public int numEnemigos;
+    public List<Transform> losePositions;
 
 
     private float cuentaTiempo = 0;
     private bool haTerminado = false;
-    private int contador = 0;
+    private int contadorEnemigos = 0;
 
     public void OnTriggerEnter(Collider other)
     {
-        if (!haTerminado)
-        {
+        //if (!haTerminado)
+        //{
             if (other.gameObject.CompareTag("Player"))
             {
                 PerderFuncion();
             }
             if (other.gameObject.CompareTag("enemy"))
             {
-                contador++;
+                contadorEnemigos++;
 
-                if (contador == numEnemigos)
+                SendEnemyToStart( other.gameObject );
+
+                if (contadorEnemigos == numEnemigos)
                 {
                     GanarFuncion();
                 }
             }
-        }
+        //}
     }
 
     public void Update()
@@ -57,5 +61,16 @@ public class KillZone : MonoBehaviour
         hasGanadoText.text = "hasGanado en " + cuentaTiempo + " segundos";
         hasGanado.SetActive(true);
         haTerminado = true;
+    }
+
+
+    public void SendEnemyToStart( GameObject currentEnemy )
+    {
+        currentEnemy.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        currentEnemy.GetComponent<Rigidbody>().isKinematic  = true;
+        currentEnemy.GetComponent<CapsuleCollider>().enabled = false;
+        currentEnemy.GetComponent<movimientoPJ>().enabled = false;
+
+        currentEnemy.transform.DOMove( losePositions[contadorEnemigos-1].position, 1 );
     }
 }
