26f741cd6f75d7f1829c26fbe984cd06e77a5850
JoseL90 <joseluisgc90trabajo@gmail.com>
2024-05-31 02:45:10 +0200

    audios listos

--- Assets/Scripts/Colisiones.cs
+++ Assets/Scripts/Colisiones.cs
@@ -5,26 +5,11 @@ using UnityEngine;
 public class Colisiones : MonoBehaviour
 {
     public AudioSource audioColision;
-    // Este método se llama cuando el colisionador entra en contacto con otro colisionador
+    // Metodo colisionador
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag =="enemy"){
-            Debug.Log("Colisionó con " + collision.gameObject.name);
             audioColision.Play();
         }
     }
-    /*
-    // Este método se llama mientras el colisionador está en contacto con otro colisionador
-    private void OnCollisionStay(Collision collision)
-    {
-        // Puedes usar esto para detectar colisiones continuas
-        Debug.Log("Manteniendo colisión con " + collision.gameObject.name);
-    }
-
-    // Este método se llama cuando el colisionador deja de estar en contacto con otro colisionador
-    private void OnCollisionExit(Collision collision)
-    {
-        // Imprime un mensaje cuando se termina la colisión
-        Debug.Log("Dejó de colisionar con " + collision.gameObject.name);
-    }*/
 }
